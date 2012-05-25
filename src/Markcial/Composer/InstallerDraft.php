c3215a3b5d28c37bfaf0f79c844bee6d9aeff04b
Marc <markcial@gmail.com>
2012-05-25 14:33:49 +0300

    Update src/Markcial/Composer/InstallerDraft.php

--- src/Markcial/Composer/InstallerDraft.php
+++ src/Markcial/Composer/InstallerDraft.php
@@ -8,17 +8,47 @@ class InstallerDraft implements IInstallerDraft
 {
 	public static function preInstall( Event $event )
 	{
-		var_dump( $event );
-        file_put_contents( '/tmp/evt_data.dat', var_export( $event, true ) );
-        die;
+		 $composer = $event->getComposer();
+		 $operation = $composer->getOperation();
+		 $package = $operation->getPackage();
+		 
+		 echo $operation;
+		 echo $package;
 	}
-	public static function postInstall( Event $event ){}
-	public static function preUpdate( Event $event ){}
-	public static function postUpdate( Event $event ){}
-	public static function prePackageInstall( Event $event ){}
-	public static function postPackageInstall( Event $event ){}
-	public static function prePackageUpdate( Event $event ){}
-	public static function postPackageUpdate( Event $event ){}
-	public static function prePackageUninstall( Event $event ){}
-	public static function postPackageUninstall( Event $event ){}
+	public static function postInstall( Event $event )
+	{
+		
+	}
+	public static function preUpdate( Event $event )
+	{
+		
+	}
+	public static function postUpdate( Event $event )
+	{
+		
+	}
+	public static function prePackageInstall( Event $event )
+	{
+		
+	}
+	public static function postPackageInstall( Event $event )
+	{
+		
+	}
+	public static function prePackageUpdate( Event $event )
+	{
+		
+	}
+	public static function postPackageUpdate( Event $event )
+	{
+		
+	}
+	public static function prePackageUninstall( Event $event )
+	{
+		
+	}
+	public static function postPackageUninstall( Event $event )
+	{
+		
+	}
 }
